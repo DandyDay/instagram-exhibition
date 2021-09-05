7b051e24abb63eee5137c371f67d209bfba6a63c
Jinho Choi <dandyday31@gmail.com>
2021-09-05 19:55:03 +0900

    210905_1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import Photo from "./photo";
 import "./styles.css";
+import "./App.css";
+
+const ACCESS_TOKEN =
+  "IGQVJYMFRRZAHh5VjFramtDcm1XUk5QMlFCSVNHQWwtNUthbm5XT1RhUmhlVElzbGwyellnbGw1dXloUW5FMXJtNnpJVm53d2g3SjFDNjNMNGNTSlhMUk9YZAFhKTGZATMWlUeDlZATk1n";
+
+function useFetch(url) {
+  const [error, setError] = useState("");
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const callUrl = async () => {
+    try {
+      const {
+        data: { data }
+      } = await Axios.get(url);
+      setPayload(data);
+    } catch {
+      setError("Error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    callUrl();
+  });
+
+  return { payload, loading, error };
+}
 
 export default function App() {
+  const { payload, loading, error } = useFetch(
+    `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,username,timestamp&access_token=${ACCESS_TOKEN}`
+  );
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {loading && <span>Loading</span>}
+      {!loading && error && <span>{error}</span>}
+      <div className="articles">
+        {!loading &&
+          payload &&
+          payload.map((photo) => {
+            if (photo.media_type === "IMAGE") {
+              return (
+                <Photo
+                  key={photo.id}
+                  id={photo.id}
+                  caption={photo.caption}
+                  media_url={photo.media_url}
+                  username={photo.username}
+                  timestamp={photo.timestamp}
+                />
+              );
+            } else if (photo.media_type === "CAROUSEL_ALBUM") {
+              return <span key={photo.id}>{photo.id}</span>;
+            }
+            return null;
+          })}
+      </div>
     </div>
   );
 }
---
+++ src/photo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Photo({ id, caption, media_url, username, timestamp }) {
+  return (
+    <article id={id} className="article">
+      <img className="photo" alt={username} src={media_url} />
+      <div className="username"> {username} </div>
+      <div className="caption">{caption}</div>
+      <div className="timestamp">
+        <div className="timestamp_date">{timestamp.substr(0, 10)}</div>
+        &nbsp;
+        <div className="timestamp_time">{timestamp.substr(11, 5)}</div>
+      </div>
+    </article>
+  );
+}
+
+export default Photo;
